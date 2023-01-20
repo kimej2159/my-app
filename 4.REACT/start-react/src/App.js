d0bdc86a18f411772ef4177e35ad638b184ed75a
kimej2159 <kimej2159@naver.com>
2023-01-20 12:54:56 +0900

    단위환산 앱 개발 완료

--- 4.REACT/start-react/src/App.js
+++ 4.REACT/start-react/src/App.js
@@ -5,14 +5,20 @@ function App(){
   // 화살함수
   const convert = () => {console.log("단위를 min에서  hours로 변경합니다")};
   const [amount, setAmount] = useState("0");
-  const handleSubmit = (e) => e.preventDefault();
+  const [disabled, setDisabled] = useState(false);
+
 
   //일반함수
   function handleChange(event){
     setAmount(event.target.value);
   }
 
-  const resetInput = () => setAmount("0")
+  const resetInput = () => {
+    setAmount("0");
+    setDisabled(false);   //반전 누르면 disabled가 true로 바뀌는데, 이때 reser누르면 disabled를 초기화
+  } 
+  const handleSubmit = (e) => e.preventDefault();
+  const inversion = () => setDisabled((currenteunji) => !currenteunji);   //current : state의 이전 값을 알아서 가졍는 파라키터 명
   return(
     <div class="main">
       <h1 className='site-header'>단위 환산 앱</h1>
@@ -23,9 +29,28 @@ function App(){
         <div className="converter">
           <form onSubmit={handleSubmit}>
           <ul>
-            <li><input type="text" value={amount} onChange={handleChange} placeholder="값을 입력하세요"  />minutes</li>
-            <li><input type="text"  value={amount / 60} placeholder="값을 입력하세요"  />hours</li>
-            <li><button onClick={resetInput}>리셋</button></li>
+            <li>
+              <input 
+                type="text"
+                value={disabled ? amount * 60 : amount} 
+                placeholder="값을 입력하세요"
+                onChange={handleChange} 
+                disabled={disabled} />
+              <label>minutes</label>
+            </li>
+            <li>
+              <input
+                type="text"  
+                value={!disabled ? amount / 60 : amount} 
+                placeholder="값을 입력하세요"
+                onChange={handleChange} 
+                disabled={!disabled} />
+              <label>hours</label>
+            </li>
+            <li>
+              <button onClick={resetInput}>초기화</button>
+              <button onClick={inversion}>반전</button>
+            </li>
           </ul>
           </form>
         </div>
